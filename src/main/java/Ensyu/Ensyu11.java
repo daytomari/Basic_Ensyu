395ecb2bffe009c48c7a859fc88292116c044e72
daytomari <tomari.daigorou@link-academy.co.jp>
2024-06-18 17:58:35 +0900

    初回コミット

---
+++ src/main/java/Ensyu/Ensyu11.java
@@ -0,0 +1,18 @@
+package Ensyu;
+
+//Intellijの便利機能の課題
+//映像を再度確認しながら、ショートカットの復習を行いましょう
+
+// ダブル Shift　どこでも検索
+//　F2/Shift＋F2　コードの問題間を移動する・次または前のハイライトされたエラーに移動します。
+//　Ctrl Alt L　コードの整形
+//　Alt F7　使用箇所の検索。プロジェクト全体でコード要素が使用されているすべての場所を表示します。
+
+
+public class Ensyu11 {
+  public static void main(String[] args){
+// 上記ショートカット
+
+  }
+
+}
