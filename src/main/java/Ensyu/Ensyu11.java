a14dc780bb69532a1db776d9d7eb4f74f8eeea3c
daytomari <tomari.daigorou@link-academy.co.jp>
2024-06-21 17:06:50 +0900

    一部修正

--- src/main/java/Ensyu/Ensyu11.java
+++ src/main/java/Ensyu/Ensyu11.java
@@ -8,6 +8,9 @@ package Ensyu;
 //　Ctrl Alt L　コードの整形
 //　Alt F7　使用箇所の検索。プロジェクト全体でコード要素が使用されているすべての場所を表示します。
 
+//　Ctrl + B　定義されている変数や関数の定義もとにジャンプ
+//　Ctrl + Alt + ←　ジャンプ先から戻る
+//　Ctrl + Alt + →　ジャンプ先に戻る
 
 public class Ensyu11 {
   public static void main(String[] args){
