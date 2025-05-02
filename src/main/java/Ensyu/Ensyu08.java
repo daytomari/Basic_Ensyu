6f98a28bc543d6c4e25c1260080d6d9d42e6162c
DaigoroTomari <tomari.daigorou@link-academy.co.jp>
2025-05-02 11:21:00 +0900

    課題8　書き込む文字に改行を追加

--- src/main/java/Ensyu/Ensyu08.java
+++ src/main/java/Ensyu/Ensyu08.java
@@ -15,12 +15,11 @@ public class Ensyu08 {
   public static void main(String[] args) throws IOException {
     try {
       Path path = Path.of("JavaCourse.txt");
-      Files.writeString(path, "Javaコース楽しい！", StandardOpenOption.APPEND);
+      Files.writeString(path, "Javaコース楽しい！\n", StandardOpenOption.APPEND);
     } catch (IOException e) {
       System.out.println("例外が発生しました。");
     } finally {
       System.out.println("無事に処理が終了しました。");
     }
   }
-
 }
